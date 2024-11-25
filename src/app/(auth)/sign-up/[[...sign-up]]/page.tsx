23808497e84bf0cc910247516bf80e4e3bd7d83a
gourab8389 <deyg6988@gmail.com>
2024-11-25 17:53:20 +0530

    add User model to Prisma schema and update sign-in/sign-up pages for improved layout

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className="w-full max-w-screen-2xl flex items-center justify-center">
+    <div className="w-full max-w-screen-2xl min-h-screen flex items-center justify-center">
         <SignUp />
     </div>
   )
