d11073cddcd18f99298ea5eda7887f1ed17c0341
gourab8389 <deyg6988@gmail.com>
2024-11-25 17:16:38 +0530

    add Clerk integration for authentication and update home page with button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 
 export default async function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Button>
+        Click me
+      </Button>
     </div>
   );
 }
