a57b632af39890ad4c57d76f09fad0f9db86dbe8
gourab8389 <deyg6988@gmail.com>
2024-11-27 01:05:36 +0530

    enhance AppSidebar with navigation items and improved layout

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -1,15 +1,72 @@
-"use client"
+"use client";
 
-import { Sidebar, SidebarHeader } from "@/components/ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meetings",
+    icon: Presentation,
+  },
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  },
+];
 
 const AppSidebar = () => {
+    const pathname = usePathname();
   return (
     <Sidebar collapsible="icon" variant="floating">
-        <SidebarHeader>
-            Logo
-        </SidebarHeader>
+      <SidebarHeader>Logo</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-2">
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                    <Link href={item.url} className={cn(
+                        {"!bg-primary !text-white": pathname === item.url}, "list-none p-6"
+                        )}>
+                            <item.icon />
+                            <span>{item.title}</span>
+                    </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
-  )
-}
+  );
+};
 
 export default AppSidebar;
