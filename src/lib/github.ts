a784058b57344062c0c604c888655ff2caadf88c
gourab8389 <deyg6988@gmail.com>
2024-11-29 00:45:30 +0530

    add Commit model to schema and implement getCommitHashes function for fetching recent commits from GitHub

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,3 +3,31 @@ import { Octokit } from "octokit";
 export const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
+
+const githubUrl = "https://github.com/docker/genai-stack"
+
+type Response = {
+    commitHash: string;
+    commitMessage: string;
+    commitAuthorName: string;
+    commitAuthorAvatar: string;
+    commitdate: string;
+}
+
+export const getCommitHashes = async (githubUrl: string): Promise<Response[]> => {
+    const { data } = await octokit.rest.repos.listCommits({
+        owner: "docker",
+        repo: "genai-stack",
+    })
+    const sortedCommits = data.sort((a: any, b: any) => new Date(b.commit.author.date).getTime() - new Date(a.commit.author.date).getTime()) as any[]
+
+    return sortedCommits.slice(0, 15).map((commit: any)=> ({
+        commitHash: commit.sha as string,
+        commitMessage: commit.commit.message ?? "",
+        commitAuthorName: commit.commit?.author?.name ?? "",
+        commitAuthorAvatar: commit?.author?.avatar_url ?? "",
+        commitdate: commit.commit?.author?.date ?? "",
+    }))
+}
+
+getCommitHashes(githubUrl)
